188e6ed8d271f6af71f3842600239ac2ea018777
Eliweton Santos <weltonguita@yahoo.com.br>
2022-04-18 23:14:50 -0300

    creating items

--- javascript.js
+++ javascript.js
@@ -1,26 +1,56 @@
 let formList = document.getElementById('formList');
+let items = ['Eliwelton' , 'Adriana', 'Maria Eduarda'];
 
+// adding a item no array
+function addItem(item) {
+    items.push(item);
+    printItems();
+}
 
+// Capture event click
 formList.addEventListener("submit", (e) => {
     e.preventDefault();
     let item = document.getElementById('item')
-    createItem(item.value);
-    item.value = "";
+    addItem(item.value)
 });
 
-// adding a item in list
+// creating a item in list
 function createItem(item) {
+    // get element
     let list = document.getElementById('list');
     let btnList = document.createElement('button');
-    btnList.innerHTML = 'Excluir';
     let divList = document.createElement('div');
-    divList.setAttribute('class', 'list')
     let itemList = document.createElement('li');
+    // adding values and properties
+    btnList.innerHTML = 'Excluir';
+    divList.setAttribute('class', 'list')
     itemList.innerHTML = item;
     divList.appendChild(itemList)
     divList.appendChild(btnList);
     list.appendChild(divList);
 }
+
+// print element in body
+function printItems(){
+    items.forEach(item => {
+        createItem(item);
+    });
+}
+
+// delete a item on array
+function deleteItem(item) {
+    
+    items.forEach(element => {
+        let findedItem;
+        if (element == item) {
+            findedItem = element;
+            console.log(`${item} finded`);
+        } else {
+            console.log('item find not!')
+        }
+    });
+}
+
 
 
 
